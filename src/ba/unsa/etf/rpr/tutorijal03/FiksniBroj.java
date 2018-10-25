b40f85056d94940fcae6ebd966b6bcef0b45ef1e
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 21:50:49 +0200

    Dodan enum Grad

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -2,13 +2,17 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj extends  TelefonskiBroj{
 
+
+
     @Override
     public String ispisi() {
-
+        return null;
     }
 
     @Override
     public int hashCode() {
-
+        return 0;
     }
 }
+
+enum Grad{SARAJEVO, ORAŠJE, ZENICA, LIVNO, TUZLA, MOSTAR, BIHAĆ, GORAŽDE, ŠIROKI_BRIJEG, TRAVNIK, BRČKO}
