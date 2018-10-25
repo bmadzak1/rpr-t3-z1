62e64fa0bb626ff85a5f3ccefd7387e8004870fc
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 22:03:59 +0200

    Dodana metoda Ispisi za FiksniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -14,13 +14,52 @@ public class FiksniBroj extends  TelefonskiBroj{
 
     @Override
     public String ispisi() {
-        return null;
+        String fiksni = new String();
+        switch (grad){
+            case SARAJEVO:
+                fiksni += "033";
+                break;
+            case ORASJE:
+                fiksni += "031";
+                break;
+            case ZENICA:
+                fiksni += "032";
+                break;
+            case LIVNO:
+                fiksni += "034";
+                break;
+            case TUZLA:
+                fiksni += "035";
+                break;
+            case MOSTAR:
+                fiksni += "036";
+                break;
+            case BIHAC:
+                fiksni += "037";
+                break;
+            case GORAZDE:
+                fiksni += "038";
+                break;
+            case SIROKI_BRIJEG:
+                fiksni += "039";
+                break;
+            case TRAVNIK:
+                fiksni += "030";
+                break;
+            case BRCKO:
+                fiksni += "049";
+                break;
+        }
+
+        fiksni += "/";
+        fiksni += broj;
+        return fiksni;
     }
 
     @Override
     public int hashCode() {
         return 0;
     }
-
 }
+
 
