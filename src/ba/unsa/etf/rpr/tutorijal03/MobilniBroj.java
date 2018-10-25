3332b6e5f90fbf4a6b0423d29e42198dfe880f1e
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 22:05:26 +0200

    Nasljeđivanje klase MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,6 +1,14 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MobilniBroj {
+public class MobilniBroj extends  TelefonskiBroj{
 
+    @Override
+    public String ispisi() {
+        return null;
+    }
 
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
