28923b46beeec70b2824d2bd2e91782c58b73cc5
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 21:45:17 +0200

    Kreirana klasa FiksniBroj koja je izvedena iz klase TelefonskiBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class FiksniBroj extends  TelefonskiBroj{
+
+    @Override
+    public String ispisi() {
+
+    }
+
+    @Override
+    public int hashCode() {
+
+    }
+}
