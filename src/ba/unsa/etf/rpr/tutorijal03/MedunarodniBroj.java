800b2edb3a868dd71961e26ca35af7e0fd669814
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 22:09:54 +0200

    Dodan konstruktor za klasu MedunarodniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -2,6 +2,14 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class MedunarodniBroj extends TelefonskiBroj{
 
+    String drzava;
+    String broj;
+
+    MedunarodniBroj(String drzava, String broj){
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
     @Override
     public String ispisi() {
         return null;
