10479aff8975f1572c04754f656eeeddcaf73ae1
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 22:06:13 +0200

    Dodan konstruktor klase MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -2,6 +2,14 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class MobilniBroj extends  TelefonskiBroj{
 
+    int mobilnaMreza;
+    String broj;
+
+    MobilniBroj(int mobilnaMreza, String broj){
+        this.mobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
+
     @Override
     public String ispisi() {
         return null;
