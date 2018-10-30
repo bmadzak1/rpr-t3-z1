68f0ed15e8ffac5a39a1fc92e4305aea86ae9e9f
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-30 15:42:34 +0100

    Dodana metoda dajBroj

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class Imenik {
     HashMap<String, TelefonskiBroj> mapa = new HashMap<String, TelefonskiBroj>();
@@ -8,5 +9,11 @@ public class Imenik {
     void dodaj(String ime, TelefonskiBroj broj){
         mapa.put(ime, broj);
     }
+
+    String dajBroj(String ime){
+        return mapa.get(ime).ispisi();
+    }
+
+
 
 }
