15a0946213415ff7975f384f70fc3257cfae9d99
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-30 15:24:01 +0100

    Dodana metoda dodaj

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.HashMap;
+
 public class Imenik {
+    HashMap<String, TelefonskiBroj> mapa = new HashMap<String, TelefonskiBroj>();
+
+    void dodaj(String ime, TelefonskiBroj broj){
+        mapa.put(ime, broj);
+    }
+
 }
