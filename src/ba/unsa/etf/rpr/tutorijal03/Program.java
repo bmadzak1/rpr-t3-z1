f3e8a8dbb347755109dadbb6a67171339926fbbb
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-30 16:34:58 +0100

    Dodan tekst u klasi Program

--- src/ba/unsa/etf/rpr/tutorijal03/Program.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
 public class Program {
+
+    Scanner ulaz = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Unesite 1 za dodavanje telefonskog broja");
+        System.out.println("Unesite 2 da dobijete broj preko imena");
+        System.out.println("Unesite 3 da dobijete ime preko broja");
+        System.out.println("Unesite 4 da ispisete sve brojeve cije ime pocinje sa nekim slovom");
+        System.out.println("Unesite 5 da ispisete imena svih osoba koje zive u nekom gradu");
+        System.out.println("Unesite 6 da ispisete brojeve svih osoba koje zive u nekom gradu");
+    }
 }
